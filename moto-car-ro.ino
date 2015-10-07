22e341cda0dca85747e160fc96c80bb8a1d8b581
Lucas Cerveró/llucbrell <llucbrell@gmail.com>
2015-10-07 09:02:57 +0200

    one motor test

---
+++ moto-car-ro.ino
@@ -0,0 +1,29 @@
+byte motorPlus = 9;
+
+ 
+
+void setup() {
+  // put your setup code here, to run once:
+
+// wake up pins!!!
+pinMode(motorPlus, OUTPUT);
+
+
+// turn-off motor
+digitalWrite(motorPlus, LOW);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //digitalWrite(motorSpeed, HIGH);
+  //analogWrite(enable, 200);
+  digitalWrite(motorPlus, HIGH); //gira a la derecha
+ 
+  delay(2000);
+  digitalWrite(motorPlus, LOW);
+ 
+  delay(2000);
+ 
+}
